339424e8ed6be00421eafb92f2509c6f7b151fa5
MateoTVara <torresvaramateo@gmail.com>
2023-11-19 14:25:26 -0500

    Implementación de la vista de cada producto

--- src/main/java/com/idat/pe/controller/HomeController.java
+++ src/main/java/com/idat/pe/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.idat.pe.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import com.idat.pe.model.Producto;
 import com.idat.pe.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -19,5 +26,17 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
 	}
+	
+	@GetMapping("productoHome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id enviado como parametro {}",id);
+		
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional=productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto",producto);
+		
+		return "usuario/productoHome";	}
 	
 }
