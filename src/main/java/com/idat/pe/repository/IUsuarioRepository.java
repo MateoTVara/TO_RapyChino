815017f2d2cc2bbaf5dc8a3abe61b2f8fe00db48
MateoTVara <torresvaramateo@gmail.com>
2023-11-21 10:04:16 -0500

    Implementación de una autenticación provisional de usuarios

--- src/main/java/com/idat/pe/repository/IUsuarioRepository.java
+++ src/main/java/com/idat/pe/repository/IUsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.idat.pe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.idat.pe.model.Usuario;
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario,Integer>{
 
+	Optional<Usuario> findByEmail(String email);
+	
 }
