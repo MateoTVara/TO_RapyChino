6980951b67b8699db177d5c27e2aee8cc1f4796d
MateoTVara <torresvaramateo@gmail.com>
2023-11-21 14:05:18 -0500

    Implementación de la visualización de los detalles de cada compra

--- src/main/java/com/idat/pe/controller/UsuarioController.java
+++ src/main/java/com/idat/pe/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -81,6 +82,19 @@ public class UsuarioController {
 		model.addAttribute("pedidos", pedidos);
 
 		return "usuario/compras";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalleCompra(@PathVariable Integer id, HttpSession session, Model model) {
+		logger.info("Id del pedido: {}", id);
+		Optional<Pedido> pedido=pedidoService.findById(id);
+
+		model.addAttribute("detalles", pedido.get().getDetalle());
+
+
+		//session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "usuario/detallecompra";
 	}
 	
 }
--- src/main/java/com/idat/pe/service/IPedidoService.java
+++ src/main/java/com/idat/pe/service/IPedidoService.java
@@ -1,6 +1,7 @@
 package com.idat.pe.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.idat.pe.model.Pedido;
 import com.idat.pe.model.Usuario;
@@ -8,6 +9,7 @@ import com.idat.pe.model.Usuario;
 public interface IPedidoService {
 	
 	Pedido save(Pedido pedido);
+	Optional<Pedido> findById(Integer id);
 	List<Pedido> findAll();
 	String generarNumeroPedido();
 	List<Pedido> findByUsuario(Usuario usuario);
