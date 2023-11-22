01760c7a7aaf3a37fb5f6c5dd7710f0841d618bd
MateoTVara <torresvaramateo@gmail.com>
2023-11-22 10:50:36 -0500

    Implementación de las vistas del pedido y de detalle de los pedidos para la vista del administrador

--- src/main/java/com/idat/pe/controller/AdministradorController.java
+++ src/main/java/com/idat/pe/controller/AdministradorController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.idat.pe.model.Pedido;
 import com.idat.pe.model.Producto;
+import com.idat.pe.service.IPedidoService;
 import com.idat.pe.service.IProductoService;
 import com.idat.pe.service.IUsuarioService;
 
@@ -21,6 +24,9 @@ public class AdministradorController {
 	
 	@Autowired
 	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private IPedidoService pedidoService;
 
 	@GetMapping("")
 	public String home(Model model) {
@@ -35,6 +41,22 @@ public class AdministradorController {
 	public String usuario(Model model) {
 		model.addAttribute("usuarios", usuarioService.findAll());
 		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/pedidos")
+	public String pedidos(Model model) {
+		model.addAttribute("pedidos",pedidoService.findAll());
+		return "administrador/pedidos";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id) {
+		
+		Pedido pedido = pedidoService.findById(id).get();
+		
+		model.addAttribute("detalles", pedido.getDetalle());
+		
+		return "administrador/detallePedido";
 	}
 	
 }
